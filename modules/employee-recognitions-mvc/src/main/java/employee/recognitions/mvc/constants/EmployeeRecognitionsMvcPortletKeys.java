90e2a6b605215f36683c9eb3a30772510470c5b6
Krupasindhu Pani <krupasindhupani123@gmail.com>
2025-12-18 18:21:59 -0800

    Initial Liferay workspace with employee recognition MVC portlet

---
+++ modules/employee-recognitions-mvc/src/main/java/employee/recognitions/mvc/constants/EmployeeRecognitionsMvcPortletKeys.java
@@ -0,0 +1,11 @@
+package employee.recognitions.mvc.constants;
+
+/**
+ * @author ASUS
+ */
+public class EmployeeRecognitionsMvcPortletKeys {
+
+	public static final String EMPLOYEERECOGNITIONSMVC =
+		"employee_recognitions_mvc_EmployeeRecognitionsMvcPortlet";
+
+}
